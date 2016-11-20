1494bc1df00328835fd4962f3ce29fc352e86ba9
Shrey Gupta <shrey.gupta@duke.edu>
2016-11-20 00:02:36 -0500

    Update files

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,29 @@
+var names = [];
+var ton = [];
+var seer = [];
+var price = [];
+var vendor = [];
+var model = [];
+var url = [];
+
 function viewSavings() {
+	$.get("../static/data/acUnitData.csv", function(data) {
+		var temp = data.split('\n');
+		for(var i=1; i<temp.length; i++) {
+			var temp2 = temp[i].split(',');
+			names.push(temp2[0]);
+			ton.push(parseInt(temp2[1]));
+			seer.push(parseInt(temp2[2]));
+			price.push(parseFloat(temp2[3]));
+			vendor.push(temp2[4]);
+			model.push(temp2[5]);
+			url.push(temp2[6]);
+		}
+		helper();
+	});
+}
+
+function helper() {
 	var state = document.getElementById('autocomplete').value.split(", ")[0].toLowerCase();
 	var numOfPpl = parseInt(document.getElementById('household-size').value);
 	var remYears = parseInt(document.getElementById('years').value);
@@ -17,7 +42,30 @@ function viewSavings() {
 	acUnitStr = acUnitStr.replace("[manufacturer]", acManf);
 	acUnitStr = acUnitStr.replace("[year]", acYear);
 	acUnitStr = acUnitStr.replace("[amount]", currCostPerYear);
+	var currTotalCost = currCostPerYear*remYears;
+
+	var bestMatch = 0;
+	var bestMatchPrice = currTotalCost;
+	for(var i=0; i<names.length; i++) {
+		var newCostPerYear = calcTotalCostSEER(state, seer[i], sqFeet);
+		var newTotalCost = price[i] + newCostPerYear*remYears;
+		if(newTotalCost < bestMatchPrice) {
+			bestMatch = i;
+			bestMatchPrice = newTotalCost;
+		}
+	}
+
+	if(bestMatchPrice != currTotalCost) acUnitStr = acUnitStr.replace("[should/should not]", "should");
+	else acUnitStr.replace("[should/should not]", "should not");
 	document.getElementById('acUnitText').innerHTML = acUnitStr;
 
-	
+	document.getElementById('acUnitRows').innerHTML = "";
+	var newRow = "<tr>";
+	newRow += '<td class="text-left">' + model[bestMatch] + '</td>';
+	newRow += '<td class="text-left">$' + price[bestMatch] + '</td>';
+	newRow += '<td class="text-left">' + ton[bestMatch] + ' tons ($' + calcTotalCostSEER(state, seer[bestMatch], sqFeet) + '/year) </td>';
+	newRow += '<td class="text-left">$' + ((currTotalCost-bestMatchPrice)/remYears).toFixed(2) + '</td>';
+	newRow += '<td class="text-left"><a target="_blank" href="' + url[bestMatch] + '" class="btn btn go-slide animated" data-animation="fadeIn" data-animation-delay="60">Purchase</a></td>';
+	newRow += "</tr>";
+	document.getElementById('acUnitRows').innerHTML += newRow;
 }
