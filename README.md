09167acb85666083b92de6a42e4c20a685d7eb8e
Shrey Gupta <shrey.gupta@duke.edu>
2016-11-19 21:22:23 -0500

    Update files

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 HackDuke -- Watt
 ============
 ####Watt
-Use Watt to ---.
+Use Watt to save money and lower energy consumption for household appliances.
 
 ####More
 Developed by Shrey Gupta, Nikhil Ravi, Mihir Paithane, and Trishul Nagenalli at HackDuke (Fall 2016). Visit http://wattwatt.org for more info.
--- __init__.py
+++ __init__.py
@@ -7,35 +7,5 @@ app = Flask(__name__)
 def hello():
     return render_template("index.html")
 
-@app.route("/purchases/<my_id>")
-def purchases(my_id):
-    lst = []
-    with app.open_resource('static/data/purchases.txt') as f:
-        lines = f.readlines()
-        for i in range(len(lines)):
-            lines[i] = lines[i].split("\t")
-            lst.append({
-                    "user_id": my_id,
-                    "date": lines[i][0],
-                    "company": lines[i][1],
-                    "category": lines[i][2],
-                    "amount": int(lines[i][3])
-                })
-        return jsonify(user_purchases=lst)
-
-@app.route("/deposits/<my_id>")
-def deposits(my_id):
-    lst = []
-    with app.open_resource('static/data/deposits.txt') as f:
-        lines = f.readlines()
-        for i in range(len(lines)):
-            lines[i] = lines[i].split("\t")
-            lst.append({
-                    "user_id": my_id,
-                    "date": lines[i][0],
-                    "amount": int(lines[i][1]),
-                })
-        return jsonify(user_deposits=lst)
-
 if __name__ == "__main__":
     app.run(debug=True)
